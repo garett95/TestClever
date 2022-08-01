8f33dd8cd176914a2e569cb32fcfe51b57c70024
garett95 <7934650@gmail.com>
2022-08-01 19:54:34 +0300

    Add Test

---
+++ TestCleverence/TestCleverence/AsyncCaller.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCleverence
+{
+    class AsyncCaller
+    {
+        private EventHandler _handler;
+        public AsyncCaller(EventHandler handler)
+        {
+            _handler = handler;
+        }
+        public bool Invoke(
+            int ms,
+            object sender,
+            EventArgs e)
+        {
+            var task = Task.Factory.StartNew(() => _handler.Invoke(sender, e));
+
+            return task.Wait(ms);
+        }
+    }
+}
---
+++ TestCleverence/TestCleverence/GetAddServer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestCleverence
+{
+    class GetAddServer
+    {
+        private static int count;
+        private static ReaderWriterLockSlim readerWriterLock = new ReaderWriterLockSlim();
+        public static int GetCount()
+        {
+            readerWriterLock.EnterReadLock();
+            try
+            {
+                return count;
+            }
+            finally
+            {
+                readerWriterLock.ExitReadLock();
+            }
+        }
+        public static void AddToCount(int value)
+        {
+            readerWriterLock.EnterWriteLock();
+            try
+            {
+                checked
+                {
+                    count += value;
+                }
+            }
+            finally
+            {
+                readerWriterLock.ExitWriteLock();
+            }
+        }
+    }
+}
